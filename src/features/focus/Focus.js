050b4f5f62b79582ba7d9ee703c6f75e54fdcd62
rewpt <zpermack@gmail.com>
2022-03-31 14:06:06 -0400

    add button component to focus page

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {View, Text, StyleSheet} from 'react-native'
+import {TextInput} from "react-native-paper"
+import {RoundedButton} from '../../components/RoundedButton'
 
 export const Focus = () => {
   return (
     <View style={styles.container}> 
-      <Text>What would you like to focus on?</Text>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+        <TextInput />
+        <RoundedButton title="+" size={50}></RoundedButton>
+        </View>
+        
+      </View>
     </View>
   )
 }
@@ -13,5 +22,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 50,
+  },
+  titleContainer: {
+    flex: .5,
+    padding: 16,
+    justifyContent: "center"
+  },
+  title: {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 24
+  },
+  inputContainer: {
+    paddingTop: 20
   }
 })
