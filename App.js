dfbd200afa5c7a70c04d538cebc7bd26a80975c7
rewpt <zpermack@gmail.com>
2022-03-31 16:37:50 -0400

    refactor styles with constants

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import  {Focus} from './src/features/focus/Focus.js'
+import { colors } from './src/utils/colors.js';
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState(true);
+  const [focusSubject, setFocusSubject] = useState(null);
   return (
     <View style={styles.container}>
-      {focusSubject? <Text> hey  </Text> : null }
-      <StatusBar style="auto" />
-      <Focus />
+      {focusSubject? 
+      (<Text> Spot for a timer  </Text>) : 
+      (<Focus addSubject={setFocusSubject}/> )}
+      <Text>{focusSubject}</Text>
     </View>
   );
 }
@@ -17,7 +19,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#252250',
+    backgroundColor: colors.darkBlue,
     alignItems: 'center',
     justifyContent: 'center',
   },
