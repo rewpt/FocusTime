15b9ca546fd057cf971f7e642a90f704aae210c7
rewpt <zpermack@gmail.com>
2022-03-31 16:38:58 -0400

    add temp variable for storing text

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {View, Text, StyleSheet} from 'react-native'
 import {TextInput} from "react-native-paper"
 import {RoundedButton} from '../../components/RoundedButton'
+import {fontSizes, spacing} from "../../utils/sizes"
+import { colors } from '../../utils/colors'
 
-export const Focus = () => {
+
+export const Focus = ({addSubject}) => {
+  const [tmpItem, setTmpItem] = useState(null)
   return (
     <View style={styles.container}> 
       <View style={styles.titleContainer}>
         <Text style={styles.title}>What would you like to focus on?</Text>
         <View style={styles.inputContainer}>
-        <TextInput />
-        <RoundedButton title="+" size={50}></RoundedButton>
+        <TextInput style={{flex : 1, marginRight: spacing.md}}
+      onSubmitEditing={({nativeEvent}) => {
+        setTmpItem(nativeEvent.text)
+        console.log(tmpItem)
+      } }/>
+      <View onPress={() => console.log('shit')}>
+        <RoundedButton size={50} title="+" onPress={() => addSubject(tmpItem)}  / >
+      </View>
         </View>
         
       </View>
@@ -21,19 +31,20 @@ export const Focus = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 50,
   },
   titleContainer: {
     flex: .5,
-    padding: 16,
+    padding: spacing.md,
     justifyContent: "center"
   },
   title: {
-    color: "white",
+    color: colors.white,
     fontWeight: "bold",
-    fontSize: 24
+    fontSize: fontSizes.lg
   },
   inputContainer: {
-    paddingTop: 20
+    paddingTop: spacing.md,
+    flexDirection: 'row',
+    alignItems: 'center'
   }
 })
