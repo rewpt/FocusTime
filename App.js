0e9e83b9168ec05723195feb5cb397e05627b0e1
rewpt <zpermack@gmail.com>
2022-03-31 17:19:06 -0400

    import timer component

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 import  {Focus} from './src/features/focus/Focus.js'
 import { colors } from './src/utils/colors.js';
+import { Timer } from './src/features/timer/Timer.js';
+import { spacing } from './src/utils/sizes.js';
 
 export default function App() {
-  const [focusSubject, setFocusSubject] = useState(null);
+  const [focusSubject, setFocusSubject] = useState("gardening");
   return (
     <View style={styles.container}>
       {focusSubject? 
-      (<Text> Spot for a timer  </Text>) : 
+      (<Timer focusSubject={focusSubject}/>) : 
       (<Focus addSubject={setFocusSubject}/> )}
       <Text>{focusSubject}</Text>
     </View>
@@ -19,8 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: Platform.OS === 'ios' ? spacing.md : spacing.lg ,
     backgroundColor: colors.darkBlue,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
