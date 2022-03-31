d158d958dada4f9d1a76da181cb2c25bd0e9210d
rewpt <zpermack@gmail.com>
2022-03-31 13:28:29 -0400

    create and import focus component to app.js

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import {useState} from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import  {Focus} from './src/features/focus/Focus.js'
 
 export default function App() {
+  const [focusSubject, setFocusSubject] = useState(true);
   return (
     <View style={styles.container}>
-      <Text>Open up App.jisoo to start working on your app!</Text>
+      {focusSubject? <Text> hey  </Text> : null }
       <StatusBar style="auto" />
+      <Focus />
     </View>
   );
 }
