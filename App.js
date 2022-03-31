9ab1402bab4c9af40c31056650845d5c9e94b89f
rewpt <zpermack@gmail.com>
2022-03-31 13:31:27 -0400

    add react-native-paper dep

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#252250',
     alignItems: 'center',
     justifyContent: 'center',
   },
