10e6a4ae5feb389f0b477fd74752b891d4efc66e
rewpt <zpermack@gmail.com>
2022-04-01 12:33:52 -0400

    create countdown component and import into timer

---
+++ src/components/Countdown.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Text, View, StyleSheet} from 'react-native';
+
+export const Countdown = () => {
+  return (
+    <Text> timer here</Text>
+  )
+}
--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -2,12 +2,14 @@ import React from "react";
 import {StyleSheet, Text, View} from "react-native"
 import { colors } from "../../utils/colors";
 import { spacing, sizes } from "../../utils/sizes";
+import { Countdown } from "../../components/Countdown";
 
 export const Timer = (props) => {
 const {focusSubject} = props
 
   return (
 <View style={styles.container}>
+  <Countdown />
   <View style={{ paddingTop: spacing.xxl}}>
     <Text style={styles.title}>Focusing on:</Text>
     <Text style={styles.task}>{focusSubject}</Text>
